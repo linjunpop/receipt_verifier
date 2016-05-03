ff6a4720e8a6163e037ada91b315ddea9fc31081
Jun Lin <linjunpop@gmail.com>
2016-05-03 16:19:33 +0800

    Publish to hex.pm.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ReceiptVerifier.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -17,19 +19,26 @@ defmodule ReceiptVerifier.Mixfile do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
       {:poison, "~> 2.0"}
      ]
+  end
+
+  defp description do
+    "Verify iTunes Receipt"
+  end
+
+  defp package do
+    [
+      name: :receipt_verifier,
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Jun Lin"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/linjunpop/receipt_verifier"
+      }
+    ]
   end
 end
