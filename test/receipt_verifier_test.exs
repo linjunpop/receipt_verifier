8ef7f241160158791fc40171b34e9af903e6f592
Jun Lin <linjunpop@gmail.com>
2016-11-09 16:33:54 +0800

    Allow to verify auto renewable receipt.

--- test/receipt_verifier_test.exs
+++ test/receipt_verifier_test.exs
@@ -20,4 +20,19 @@ defmodule ReceiptVerifierTest do
       assert "1241", receipt["application_version"]
     end
   end
+
+  test "valid auto renewable receipt" do
+    use_cassette "auto_renewable_receipt" do
+      receipt_file_path = "test/fixtures/auto_renewable_receipt"
+
+      base64_receipt =
+        receipt_file_path
+        |> File.read!
+        |> String.replace("\n", "")
+
+      {:ok, receipt} = ReceiptVerifier.verify(base64_receipt)
+
+      assert "1241", receipt["application_version"]
+    end
+  end
 end
