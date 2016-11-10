a6ec9dea30b792119c747d01617c395b15895de2
Jun Lin <linjunpop@gmail.com>
2016-11-10 08:55:40 +0800

    Set latest_receipt_info's default to `[]`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,11 +2,13 @@
 
 ## master
 
+* Set latest_receipt_info's default to `[]`.
+
 ## V0.1.0
 
-* Support auto-renewable subscriptions
+* Support auto-renewable subscriptions.
 * Handle 503 Service Unavaliable.
 
 ## v0.0.1
 
-* Initial release
+* Initial release.
--- lib/receipt_verifier/receipt.ex
+++ lib/receipt_verifier/receipt.ex
@@ -1,4 +1,8 @@
 defmodule ReceiptVerifier.Receipt do
   @type t :: %__MODULE__{receipt: map, latest_receipt: binary, latest_receipt_info: [map]}
-  defstruct [:receipt, :latest_receipt, :latest_receipt_info]
+  defstruct [
+    receipt: nil,
+    latest_receipt: nil,
+    latest_receipt_info: []
+  ]
 end
