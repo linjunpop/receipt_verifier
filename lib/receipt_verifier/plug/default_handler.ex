357b13d1414ab35f020de0c230dffeabe08490ee
Jun Lin <linjunpop@gmail.com>
2017-07-26 08:32:18 +0800

    Revert "Initialize a Plug handler."
    This might be extract into another hex package.
    
    This reverts commit fe4c022c4e4b552eba0463dcbd6778bdf0f697cf.

--- lib/receipt_verifier/plug/default_handler.ex
+++
@@ -1,9 +0,0 @@
-defmodule ReceiptVerifier.Plug.DefaultHandler do
-  @behaviour ReceiptVerifier.Plug.Handler
-
-  def handle_data(_conn, data) do
-    # TODO: handle data
-    :ok
-  end
-end
-
--- lib/receipt_verifier/plug/handler.ex
+++
@@ -1,4 +0,0 @@
-defmodule ReceiptVerifier.Plug.Handler do
-
-  @callback handle_data(conn :: Plug.Conn.t, data :: map) :: :ok | {:error, any}
-end
